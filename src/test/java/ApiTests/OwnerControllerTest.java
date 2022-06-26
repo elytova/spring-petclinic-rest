295db5ffc2f4c566a86421f8f030a7a604d32da3
Evgeniya <e.lytova@tinkoff.ru>
2022-06-26 11:02:50 +0300

    ownercontroller is ready

--- src/test/java/ApiTests/OwnerControllerTest.java
+++ src/test/java/ApiTests/OwnerControllerTest.java
@@ -5,24 +5,31 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.PreparedStatement;
 
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
+import io.restassured.http.ContentType;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.*;
 
+import static Helpers.APIMethods.*;
+import static Helpers.RandomHelper.randomNumeric;
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.empty;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.not;
 
+@DisplayName("Тестирование OwnerControllerTest")
 public class OwnerControllerTest {
 
 	private static Connection connection;
+	final static int TEST_OWNER_ID = randomNumeric();
+	final static String TEST_OWNER_NAME = "Testname";
+	final static String TEST_OWNER_LASTNAME = "Testlastname";
 
 	@BeforeAll
 	public static void connect() throws SQLException {
-		connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/petclinic", "petclinic",
-				"petclinic");
+		connection = DriverManager.getConnection(
+			"jdbc:postgresql://localhost:5432/petclinic",
+			"petclinic",
+			"petclinic");
 	}
 
 	@AfterAll
@@ -30,125 +37,151 @@ public class OwnerControllerTest {
 		connection.close();
 	}
 
-	// processFindForm
+	@Nested
+	@DisplayName("processFindForm")
+	class CheckProcessFindForm {
+		@Test
+		public void shouldReturnPetsWhenGet() {
 
-	@Test
-	public void shouldReturnPetsWhenGet() {
-		when().get("/owners").then().statusCode(200).body("address", not(empty()));
-	}
+			when()
+				.get(METHOD_OWNER_WITHOUT_OWNERID)
+				.then()
+				.statusCode(200)
+				.body("address", not(empty()));
+		}
 
-	@Test
-	public void shouldReturnPetsWhenGetWithLastName() throws SQLException {
-		PreparedStatement sql = connection.prepareStatement(
+		@Test
+		public void shouldReturnPetsWhenGetWithLastName() throws SQLException {
+			PreparedStatement sql = connection.prepareStatement(
 				"INSERT INTO owners(id, first_name, last_name, address, city, telephone) VALUES(?,?,?,?,?,?)");
-		sql.setInt(1, 555);
-		sql.setString(2, "Testname");
-		sql.setString(3, "Testlastname");
-		sql.setString(4, "2698 Helloworld St.");
-		sql.setString(5, "Garden");
-		sql.setString(6, "6085539554");
-		sql.executeUpdate();
-
-		when().get("/owners/Testlastname").then().statusCode(200);
-
-		PreparedStatement sqlDelete = connection.prepareStatement("DELETE FROM country WHERE id = 555");
-		sqlDelete.executeUpdate();
+			sql.setInt(1, TEST_OWNER_ID);
+			sql.setString(2, TEST_OWNER_NAME);
+			sql.setString(3, TEST_OWNER_LASTNAME);
+			sql.setString(4, "2698 Helloworld St.");
+			sql.setString(5, "Garden");
+			sql.setString(6, "6085539554");
+			sql.executeUpdate();
+			given()
+				.queryParam("lastName", TEST_OWNER_LASTNAME).queryParam("page", 1)
+			.when().get(METHOD_OWNER_WITHOUT_OWNERID).then().statusCode(200);
+		}
 	}
 
-	// processCreationForm
-
-	@Test
-	public void shouldReturnOkWhenCreateNewUser() throws SQLException {
-		given().contentType("application/json")
+	@Nested
+	@DisplayName("processCreationForm")
+	class CheckProcessCreationForm {
+		@Test
+		public void shouldReturnOkWhenCreateNewOwner() {
+			given().contentType("application/json")
 				.body("{\n" + "  \"address\": \"testAddress\",\n" + "  \"city\": \"testcity\",\n"
-						+ "  \"firstName\": \"stringFirstName\",\n" + "  \"id\": 999,\n"
-						+ "  \"lastName\": \"stringLastName\",\n" + "  \"pets\": [\n" + "    {\n"
-						+ "      \"birthDate\": \"2000-06-20\",\n" + "      \"id\": 88,\n"
-						+ "      \"name\": \"stringName\",\n" + "      \"visits\": [\n" + "        {\n"
-						+ "          \"date\": \"2000-06-20\",\n" + "          \"description\": \"stringName\",\n"
-						+ "          \"id\": 77\n" + "        }\n" + "      ]\n" + "    }\n" + "  ],\n"
-						+ "  \"telephone\": \"2345363\"\n" + "}")
-				.when().post("/api/countries").then().statusCode(201).body("id", not(empty()));
+					+ "  \"firstName\": \"stringFirstName\",\n" + "  \"id\": " + TEST_OWNER_ID + ",\n"
+					+ "  \"lastName\": \"stringLastName\",\n" + "  \"pets\": [\n" + "    {\n"
+					+ "      \"birthDate\": \"2000-06-20\",\n" + "      \"id\": 88,\n"
+					+ "      \"name\": \"stringName\",\n" + "      \"visits\": [\n" + "        {\n"
+					+ "          \"date\": \"2000-06-20\",\n" + "          \"description\": \"stringName\",\n"
+					+ "          \"id\": 77\n" + "        }\n" + "      ]\n" + "    }\n" + "  ],\n"
+					+ "  \"telephone\": \"2345363\"\n" + "}")
+				.when().post(METHOD_OWNER_WITHOUT_OWNERID).then().statusCode(201).body("id", not(empty()));
+		}
 
-		PreparedStatement sqlDelete = connection.prepareStatement("DELETE FROM country WHERE id = 999");
-		sqlDelete.executeUpdate();
-	}
-
-	@Test
-	public void shouldReturnErrorWhenCreateWithoutBody() {
-		given().contentType("application/json").body("{}").when().post("/api/countries").then().statusCode(400)
+		@Test
+		public void shouldReturnErrorWhenCreateOwnerWithoutBody() {
+			given()
+				.contentType("application/json")
+				.body("{}")
+				.when()
+				.post(METHOD_OWNER_WITHOUT_OWNERID)
+				.then()
+				.statusCode(400)
 				.body("error", not(empty()));
+		}
 	}
 
-	// showOwner
-
-	@Test
-	public void shouldReturnPetsWhenGetShowOwnerWithId() throws SQLException {
-		PreparedStatement sql = connection.prepareStatement(
+	@Nested
+	@DisplayName("showOwner")
+	class CheckShowOwner {
+		@Test
+		public void shouldReturnPetsWhenGetShowOwnerWithId() throws SQLException {
+			PreparedStatement sql = connection.prepareStatement(
 				"INSERT INTO owners(id, first_name, last_name, address, city, telephone) VALUES(?,?,?,?,?,?)");
-		sql.setInt(1, 666);
-		sql.setString(2, "Testname");
-		sql.setString(3, "Testlastname");
-		sql.setString(4, "2698 Helloworld St.");
-		sql.setString(5, "Garden");
-		sql.setString(6, "7485539554");
-		sql.executeUpdate();
+			sql.setInt(1, TEST_OWNER_ID);
+			sql.setString(2, TEST_OWNER_NAME);
+			sql.setString(3, TEST_OWNER_LASTNAME);
+			sql.setString(4, "2698 Helloworld St.");
+			sql.setString(5, "Garden");
+			sql.setString(6, "7485539554");
+			sql.executeUpdate();
 
-		when().get("/owners/666").then().statusCode(200).body("id", not(empty()));
+			given()
+				.contentType(ContentType.JSON).pathParam("ownerId", TEST_OWNER_ID)
+			.when().get(METHOD_OWNER_WITH_OWNERID)
+			.then().statusCode(200)
+				.body("isEmpty()", Matchers.is(false)).body("id", not(empty()));
+		}
 
-		PreparedStatement sqlDelete = connection.prepareStatement("DELETE FROM country WHERE id = 666");
-		sqlDelete.executeUpdate();
+		@Test
+		public void shouldReturnPetsWhenGetShowOwnerWithoutId() {
+			when().get(METHOD_OWNER_WITHOUT_OWNERID).then().statusCode(200);
+		}
+
+		@Test
+		@Disabled
+		public void shouldReturnErrorWhenGetWithWringId() {
+			when().get("/owners/123456")
+			.then().statusCode(200).body("isEmpty()", Matchers.is(true));
+		}
 	}
 
-	@Test
-	public void shouldReturnPetsWhenGetShowOwnerWithoutId() {
-		when().get("/owners/").then().statusCode(500);
-	}
-
-	@Test
-	@Disabled
-	public void shouldReturnErrorWhenGetWithWringId() {
-		when().get("/owners/123456").then().statusCode(400);
-	}
-
-	// processUpdateOwnerForm
-	@Test
-	public void shouldChangeDataWhenUpdate() throws SQLException {
-		PreparedStatement sql = connection.prepareStatement(
+	@Nested
+	@DisplayName("processUpdateOwnerForm")
+	class CheckProcessUpdateOwnerForm {
+		@Test
+		public void shouldChangeDataWhenUpdate() throws SQLException {
+			PreparedStatement sql = connection.prepareStatement(
 				"INSERT INTO owners(id, first_name, last_name, address, city, telephone) VALUES(?,?,?,?,?,?)");
-		sql.setInt(1, 777);
-		sql.setString(2, "Testname");
-		sql.setString(3, "Testlastname");
-		sql.setString(4, "2698 Helloworld St.");
-		sql.setString(5, "Garden");
-		sql.setString(6, "7485539554");
-		sql.executeUpdate();
+			sql.setInt(1, TEST_OWNER_ID);
+			sql.setString(2, TEST_OWNER_NAME);
+			sql.setString(3, TEST_OWNER_LASTNAME);
+			sql.setString(4, "2698 Helloworld St.");
+			sql.setString(5, "Garden");
+			sql.setString(6, "7485539554");
+			sql.executeUpdate();
 
-		given().contentType("application/json").body("{\n" + "  \"address\": \"test\"\n" + "}").when()
-				.patch("/api/countries/777").then().statusCode(200).body("countryName", equalTo("test"));
+			given().contentType("application/json").pathParam("ownerId", TEST_OWNER_ID)
+				.body("{\n" + "  \"address\": \"test\"\n" + "}")
+				.when().post(METHOD_OWNER_WITH_OWNERID)
+				.then().statusCode(200)
+				.body("address", equalTo("test"));
+		}
 
-		PreparedStatement sqlDelete = connection.prepareStatement("DELETE FROM country WHERE id = 777");
-		sqlDelete.executeUpdate();
-	}
+		@Test
+		public void shouldReturnErrorWhenUpdateWithoutBody() throws SQLException {
 
-	@Test
-	public void shouldReturnErrorWhenUpdateWithoutBody() throws SQLException {
-
-		PreparedStatement sql = connection.prepareStatement(
+			PreparedStatement sql = connection.prepareStatement(
 				"INSERT INTO owners(id, first_name, last_name, address, city, telephone) VALUES(?,?,?,?,?,?)");
-		sql.setInt(1, 888);
-		sql.setString(2, "Testname");
-		sql.setString(3, "Testlastname");
-		sql.setString(4, "2698 Helloworld St.");
-		sql.setString(5, "Garden");
-		sql.setString(6, "7485539554");
-		sql.executeUpdate();
+			sql.setInt(1, 888);
+			sql.setString(2, TEST_OWNER_NAME);
+			sql.setString(3, TEST_OWNER_LASTNAME);
+			sql.setString(4, "2698 Helloworld St.");
+			sql.setString(5, "Garden");
+			sql.setString(6, "7485539554");
+			sql.executeUpdate();
 
-		given().contentType("application/json").body("{}").when().post("/api/countries/888").then().statusCode(200)
+			given().contentType("application/json").pathParam("ownerId", 888)
+				.body("{}")
+				.when().post(METHOD_OWNER_WITH_OWNERID)
+				.then().statusCode(400)
 				.body("error", not(empty()));
-
-		PreparedStatement sqlDelete = connection.prepareStatement("DELETE FROM country WHERE id = 888");
-		sqlDelete.executeUpdate();
+		}
 	}
 
+	@AfterEach
+	public void deleteDataFromDB() throws SQLException {
+		PreparedStatement sql = connection.prepareStatement(
+			"DELETE FROM OWNERS WHERE ID=? OR FIRST_NAME=?"
+		);
+		sql.setInt(1, TEST_OWNER_ID);
+		sql.setString(2, TEST_OWNER_NAME);
+		sql.executeUpdate();
+	}
 }
--- src/test/java/Helpers/APIMethods.java
+++ src/test/java/Helpers/APIMethods.java
@@ -5,4 +5,6 @@ public class APIMethods {
 	public final static String METHOD_WITH_PETID = "/owners/{ownerId}/pets/{petId}";
 	public final static String METHOD_VETS = "/vets";
 	public final static String METHOD_VISIT = "/owners/{ownerId}/pets/{petId}/visits";
+	public final static String METHOD_OWNER_WITHOUT_OWNERID = "/owners";
+	public final static String METHOD_OWNER_WITH_OWNERID = "/owners/{ownerId}";
 }
